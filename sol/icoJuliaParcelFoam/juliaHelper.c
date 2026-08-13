914dd5a3150db14f72fdeeba1878d3dc36acfdfc
Sergey Lesnik <sergey.lesnik@wikki-gmbh.de>
2026-08-13 11:11:47 +0200

    Give the Julia helpers the licence header the rest of the tree carries
    juliaHelper.c and .h were the only sources of this project's own that went out
    without one, while juliaWrapper.C and .H beside them have carried theirs since
    2024.  The equation fragments and createFields.H have none either, but that is
    upstream OpenFOAM's own convention for an included fragment and is left alone.
    
    Comments only; the compiler sees no change.

--- sol/icoJuliaParcelFoam/juliaHelper.c
+++ sol/icoJuliaParcelFoam/juliaHelper.c
@@ -1,3 +1,36 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    SPDX-License-Identifier: GPL-3.0-or-later
+
+    Copyright (C) 2026 Sergey Lesnik
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Evaluating Julia code and fetching a C-callable function pointer, with
+    a Julia exception rendered as it would be at the prompt.
+
+\*---------------------------------------------------------------------------*/
+
 #include <julia.h>
 #include <stdio.h>
 #include <stdlib.h>
